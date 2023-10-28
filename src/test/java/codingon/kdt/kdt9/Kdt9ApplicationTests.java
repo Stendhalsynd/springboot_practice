e92f6c8f6fef058cee0bbc55dc2e6fc193edf903
Stendhalsynd <qmflf556@naver.com>
2023-10-28 09:40:45 +0900

    feat: initial project setting

---
+++ src/test/java/codingon/kdt/kdt9/Kdt9ApplicationTests.java
@@ -0,0 +1,13 @@
+package codingon.kdt.kdt9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kdt9ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
